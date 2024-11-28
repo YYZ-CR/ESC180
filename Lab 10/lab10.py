f5c5a2a7ff3ab6bb288db51117656c5bdee07228
YYZ <yyz101306@gmail.com>
2024-11-28 01:40:56 -0500

    lab 10

--- Lab 10/lab10.py
+++ Lab 10/lab10.py
@@ -34,9 +34,23 @@ def sum_digits(number):
     else:
         return int(str(number)[0]) + sum_digits(str(number)[1:])
     
+import copy
+# problem 3
+def list_to_sublists(L, elements):
+    output_list = []
+    new_list = []
+    for i in range(len(L)):
+        if L[i] not in elements:
+            new_list.append(L[i])
+        else:
+            output_list.append(copy.copy(new_list))
+            new_list.clear()
+    output_list.append(copy.copy(new_list))
+    return output_list
 
 def main():
     print(sum_digits(123454321))
+    print(list_to_sublists([1,2,3,4,5,6,7], [3,6]))
 
 if __name__ == '__main__':
     main()
